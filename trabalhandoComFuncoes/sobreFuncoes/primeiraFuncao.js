450aa2a659c14e49410980cf87890c9d6ade12fc
JoaoBorgesAJ <124202683+JoaoBorgesAJ@users.noreply.github.com>
2024-03-22 19:38:50 -0300

    Node.js

---
+++ trabalhandoComFuncoes/sobreFuncoes/primeiraFuncao.js
@@ -0,0 +1,10 @@
+torrar()
+injetarPao()
+function torrar() {
+    console.log("Torrando Pão")
+}
+
+function injetarPao() {
+    console.log("Preparando para injetar o pão")
+    console.log("Finalizado")
+} 
