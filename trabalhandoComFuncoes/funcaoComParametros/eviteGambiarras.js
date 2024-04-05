060293a85ecde712e00ea5b8e6801be30a82011c
JoaoBorgesAJ <124202683+JoaoBorgesAJ@users.noreply.github.com>
2024-04-05 15:05:44 -0300

    NodeJS

---
+++ trabalhandoComFuncoes/funcaoComParametros/eviteGambiarras.js
@@ -0,0 +1,7 @@
+torrar(" Pão na chapa", undefined, "Silva")
+
+function torrar(pao, valor = 20.0, nome) {
+    console.log("Torrada feica com " + pao)
+    console.log("Ele é um pedido de " + nome)
+    console.log("O valor total é " + valor)
+}
---
+++ trabalhandoComFuncoes/funcaoComParametros/funcoesParametros.js
@@ -0,0 +1,7 @@
+torrar(" Pão integral", " Silva", 10.9)
+
+function torrar(pao, nome = " Cliente", valor) {
+    console.log("Torrada feita com" + pao)
+    console.log("ela é um pedido de" + nome)
+    console.log("O valor total e R$ " + valor)
+}
